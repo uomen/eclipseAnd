91786fee97068d77da5c047ddbdb44200920f377
qimeng <qimeng@163.com>
2017-07-30 17:04:28 +0800

    这是漆猛的第三次尝试

--- src/git01/Test02.java
+++ src/git01/Test02.java
@@ -1,6 +1,6 @@
 package git01;
 
-public class test02 {
+public class Test02 {
 
 	public static void main(String[] args) {
 		System.out.println("第一次尝试 协作开发");
---
+++ src/git01/Test03.java
@@ -0,0 +1,9 @@
+package git01;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		System.out.println("这是测试三");
+	}
+	
+}
