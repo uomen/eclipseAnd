553c1ce11ae414aaf61380260af3a3a1a00fa5fb
qimeng <qimeng@163.com>
2017-07-30 15:24:37 +0800

    第一次尝试协作开发

--- src/git01/Test01.java
+++ src/git01/Test01.java
@@ -3,6 +3,8 @@ package git01;
 public class Test01 {
   public static void main(String[] args) {
 	System.out.println("你好");
+	
+	System.out.println("你好，郭孟祥，我是漆猛");
 }
 
 }
---
+++ src/git01/test02.java
@@ -0,0 +1,9 @@
+package git01;
+
+public class test02 {
+
+	public static void main(String[] args) {
+		System.out.println("第一次尝试 协作开发");
+	}
+	
+}
